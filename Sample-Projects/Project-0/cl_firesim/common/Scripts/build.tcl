31572a5d5acca7e3fc1219c6af67d05ed8608ee8
wmatt <matt@weingarten.org>
2026-02-09 20:51:51 -0500

    Finally fix PCIE issue with litefury not being found with lspci by fixing constraints

--- Sample-Projects/Project-0/cl_firesim/common/Scripts/build.tcl
+++ Sample-Projects/Project-0/cl_firesim/common/Scripts/build.tcl
@@ -2,6 +2,16 @@
 reset_run synth_1
 launch_runs synth_1 -jobs 6
 wait_on_run synth_1
+
+# This is litefury hack to get pcie up and running. Set constraints and then redo synthesis.
+set_property PROCESSING_ORDER NORMAL \
+  [get_files -all -of_objects [get_ips Top_xdma_0_0] *PCIE_X0Y0.xdc]
+
+
+reset_run synth_1
+launch_runs synth_1 -jobs 6
+wait_on_run synth_1
+
 launch_runs impl_1 -jobs 6 -to_step write_bitstream
 wait_on_run impl_1 
 puts "Implementation done!"
